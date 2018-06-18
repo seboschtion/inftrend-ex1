81cc76c530919632c68019855781bf428ce1b673
Taiki <Taiki@muriel-thevenazs-macbook-air.home>
2018-06-19 00:36:50 +0200

    add explosion

--- Planet Destoyer/Assets/Scripts/ControlCenter.cs
+++ Planet Destoyer/Assets/Scripts/ControlCenter.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 public class ControlCenter : MonoBehaviour
 {
     public GameObject laserBeam;
+    public GameObject planet;
+    public GameObject explosion;
     public Button fireButton;
+    private int counter;
 
     void Start()
     {
         var button = fireButton.GetComponent<Button>();
         button.onClick.AddListener(ToggleLaserBeam);
 	}
-	
+
 	void ToggleLaserBeam()
     {
-        laserBeam.SetActive(!laserBeam.activeSelf);
+      counter++;
+      laserBeam.SetActive(!laserBeam.activeSelf);
+      if (counter == 5)
+      {
+        Explode();
+        }
+	}
+
+  void Explode() {
+		explosion.SetActive(true);
+		Object.Destroy(planet);
 	}
 }
